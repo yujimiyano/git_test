d608c06a0729de11c2592388c1931b853291598a
yujimiyano <yuji.miyano@gmail.com>
2016-10-05 17:02:48 +0900

    Initial Commit

---
+++ git_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_test
+//
+//  Created by Yuji Miyano on 2016/10/05.
+//  Copyright © 2016年 Yuji Miyano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
